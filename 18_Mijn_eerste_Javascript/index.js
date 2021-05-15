19e3084d68f06619b0191dc475fe4f63fd8af6f8
AnaArroyoH <75263257+AnaArroyoH@users.noreply.github.com>
2021-05-15 14:40:39 +0200

    Add files via upload

---
+++ 18_Mijn_eerste_Javascript/index.js
@@ -0,0 +1,11 @@
+console.log("Hello Winc Academy")
+let name = 'Ana';
+console.log(name);
+let a = 4;
+let b = 4;
+console.log(+a + +b);
+console.log(+a - +b);
+console.log(+a * +b);
+let c = "4";
+let d = "4";
+console.log(+c + +d);
